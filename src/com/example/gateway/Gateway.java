0756d76f32d8b0aaa5990e0f5fb1f20e60e42e7f
Matthew McNew <matthew@mcnew.(none)>
2012-11-17 14:36:13 -0700

    Work in Progress showing gates upTown

--- src/com/example/gateway/Gateway.java
+++ src/com/example/gateway/Gateway.java
@@ -1,5 +1,7 @@
 package com.example.gateway;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -29,111 +31,147 @@ public class Gateway extends Activity {
     class PlayAreaView extends View {
     	
     	
-        private Bitmap orange;
-        private Bitmap orangeHighlight;
-        private Bitmap orangePlaced;
-        private Bitmap basket;
+        private Bitmap and,or,nand,nor,not,xor;
+        private ArrayList<Bitmap> menu; 
+        DisplayMetrics metrics;
+
         private Orange[] oranges;
         private int currentOrange = -1;
 
 		public PlayAreaView(Context context) {       	
             super(context);
+            loadImages();
             reset();
         }
 		
 		protected void onDraw(Canvas canvas) {
-		
-			canvas.drawBitmap(basket, getMeasuredWidth() - basket.getWidth(), getMeasuredHeight() - basket.getHeight(), null);
+		    drawMenu(canvas,false);
+			//canvas.drawBitmap(basket, getMeasuredWidth() - basket.getWidth(), getMeasuredHeight() - basket.getHeight(), null);
 			Log.d("canvas height",Integer.toString(getMeasuredHeight()));
-			for(int i=0; i<3; i++){ 
-				canvas.drawBitmap(oranges[i].orange, oranges[i].x, oranges[i].y, null);
-			}
+//			for(int i=0; i<3; i++){ 
+//				canvas.drawBitmap(oranges[i].orange, oranges[i].x, oranges[i].y, null);
+//			}
 		}
 		
+		private void drawMenu(Canvas canvas, Boolean grey) {		 
+			int distance = 0;
+			for(Bitmap b : menu){
+				canvas.drawBitmap(b, distance, 0, null);
+				distance = distance + b.getWidth();
+			}
+		}
+			
 		public boolean onTouchEvent(MotionEvent event) {
 		    int eventaction = event.getAction();
 
-		    switch (eventaction) {
-		        case MotionEvent.ACTION_DOWN: 
-		        	Log.d("canvas height",Integer.toString((int) event.getY()));
-		        	for(int i=0; i<3; i++){ 
-						if(oranges[i].touched( event.getX(), event.getY())  ){
-							oranges[i].orange = orangeHighlight;
-							currentOrange = i;
-							this.invalidate();
-							return true;	
-						}
-					}
-
-		            break;
-
-		        case MotionEvent.ACTION_MOVE:
-		        	if(currentOrange != -1)
-		        	{
-		        		oranges[currentOrange].x = (int) event.getX();
-		        		oranges[currentOrange].y = (int) event.getY();
-		        		this.invalidate();
-		        	}
-		            // finger moves on the screen
-		            break;
-
-		        case MotionEvent.ACTION_UP:   
-		            // finger leaves the screen
-		        	if(currentOrange != -1)
-		        	{
-		        		if(inBasket(event)){
-		        			oranges[currentOrange].orange = orangePlaced;
-		        			oranges[currentOrange].freeze();
-		        		}else {
-		        			oranges[currentOrange].orange = orange;
-		        		}
-		        		this.invalidate();
-		        		currentOrange = -1;
-		        	}
-		            break;
-		    }
-
-		    // tell the system that we handled the event and no further processing is required
-		    return true; 
+//		    switch (eventaction) {
+//		        case MotionEvent.ACTION_DOWN: 
+//		        	
+//		        	//menu touch?
+//		        	int distance = 0;
+//					for(Bitmap b : menu){
+//						if(){
+//							
+//						}
+//						canvas.drawBitmap(b, distance, 0, null);
+//						distance = distance + b.getWidth();
+//					}
+//		        	
+//		        	for(int i=0; i<3; i++){ 
+//						if(oranges[i].touched( event.getX(), event.getY())  ){
+//							oranges[i].orange = orangeHighlight;
+//							currentOrange = i;
+//							this.invalidate();
+//							return true;	
+//						}
+//					}
+//
+//		            break;
+//
+//		        case MotionEvent.ACTION_MOVE:
+//		        	if(currentOrange != -1)
+//		        	{
+//		        		oranges[currentOrange].x = (int) event.getX();
+//		        		oranges[currentOrange].y = (int) event.getY();
+//		        		this.invalidate();
+//		        	}
+//		            // finger moves on the screen
+//		            break;
+//
+//		        case MotionEvent.ACTION_UP:   
+//		            // finger leaves the screen
+//		        	if(currentOrange != -1)
+//		        	{
+//		        		if(inBasket(event)){
+//		        			oranges[currentOrange].orange = orangePlaced;
+//		        			oranges[currentOrange].freeze();
+//		        		}else {
+//		        			oranges[currentOrange].orange = orange;
+//		        		}
+//		        		this.invalidate();
+//		        		currentOrange = -1;
+//		        	}
+//		            break;
+//		    }
+//
+//		    // tell the system that we handled the event and no further processing is required
+			return true;
 		}
 		
         
         
-        private boolean inBasket(MotionEvent event) {
-        	if(event.getX() > (getMeasuredWidth() - basket.getWidth())) {
-        		if(event.getY() > (getMeasuredHeight() - (3*basket.getHeight()/4))) {
-        			return true;
-        		}
-        		
-        	}
-			return false;
-		}
+//        private boolean inBasket(MotionEvent event) {
+//        	if(event.getX() > (getMeasuredWidth() - basket.getWidth())) {
+//        		if(event.getY() > (getMeasuredHeight() - (3*basket.getHeight()/4))) {
+//        			return true;
+//        		}
+//        		
+//        	}
+//			return false;
+//		}
 
 		private void reset() {
-            
-            orange = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_launcher);
-            orangeHighlight = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_launcher);
-            orangePlaced = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_launcher);
-            basket = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_launcher);
+			and.getWidth();
 
-            oranges = new Orange[3];
-            
-            
-            DisplayMetrics metrics = new DisplayMetrics();
-            getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            
-            
-            
-            oranges[0] = new Orange(metrics.widthPixels/3,metrics.widthPixels/8,orange);
-            oranges[1] = new Orange(metrics.widthPixels/2,metrics.widthPixels/5,orange);
-            oranges[2] = new Orange(2*metrics.widthPixels/3,metrics.widthPixels/8,orange);
-            
-            
+//            oranges = new Orange[3];
+//            
+//            
+//            DisplayMetrics metrics = new DisplayMetrics();
+//            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+//       
+//            
+//            
+//            oranges[0] = new Orange(metrics.widthPixels/3,metrics.widthPixels/8,orange);
+//            oranges[1] = new Orange(metrics.widthPixels/2,metrics.widthPixels/5,orange);
+//            oranges[2] = new Orange(2*metrics.widthPixels/3,metrics.widthPixels/8,orange);
+//            
         }
+		
+		private void loadImages() {
+			metrics = new DisplayMetrics();
+			getWindowManager().getDefaultDisplay().getMetrics(metrics);
+			
+            and = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.and);
+            nand = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.nand);
+            nor = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.nor);
+            not = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.not);
+            or = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.or);
+            xor = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.xor);
+            
+            menu = new ArrayList<Bitmap>();
+            menu.add(and);
+            menu.add(nand);
+            menu.add(nor);
+            menu.add(not);
+            menu.add(or);
+            menu.add(xor);
+		}
         
         
         
